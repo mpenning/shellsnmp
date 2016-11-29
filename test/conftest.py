e1fdb2ab8bb9c96b54899aad36639672cdd9d638
mpenning <mike@pennington.net>
2016-11-29 03:31:25 -0600

    Add more unit tests

--- test/conftest.py
+++ test/conftest.py
@@ -2,9 +2,8 @@ import platform
 import sys
 import os
 THIS_DIR = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(os.path.abspath(THIS_DIR), "../shellsnmp/"))
+sys.path.insert(0, os.path.join(os.path.abspath(THIS_DIR), "../"))
 sys.path.insert(0, os.path.abspath(THIS_DIR))
-
 
 import pytest
 from shellsnmp.Poller import SNMP
@@ -17,6 +16,14 @@ NETSNMP_sq_STR = """RFC1213-MIB::ifDescr.10101 = STRING: "GigabitEthernet0/1"
 RFC1213-MIB::ifDescr.10102 = STRING: "GigabitEthernet0/2"
 """
 
+NETSNMP_Oxsq_INT = """ifIndex.10101 436212224
+ifIndex.10102 436212227
+"""
+
+NETSNMP_sq_INT = """RFC1213-MIB::ifIndex.10101 = INTEGER: 436212224
+RFC1213-MIB::ifIndex.10102 = INTEGER: 436212227
+"""
+
 @pytest.yield_fixture(scope='session')
 def netsnmp_Oxsq_str(request):
     yield NETSNMP_Oxsq_STR
@@ -24,4 +31,12 @@ def netsnmp_Oxsq_str(request):
 @pytest.yield_fixture(scope='session')
 def netsnmp_sq_str(request):
     yield NETSNMP_sq_STR
+
+@pytest.yield_fixture(scope='session')
+def netsnmp_Oxsq_int(request):
+    yield NETSNMP_Oxsq_INT
+
+@pytest.yield_fixture(scope='session')
+def netsnmp_sq_int(request):
+    yield NETSNMP_sq_INT
 
--- test/test_shellsnmp_Poller.py
+++ test/test_shellsnmp_Poller.py
@@ -9,15 +9,27 @@ sys.path.insert(0, os.path.join(os.path.abspath(THIS_DIR), "../shellsnmp/"))
 from shellsnmp.Poller import SNMP
 import pytest
 
-def test_Poller_SNMP__parse_01(netsnmp_Oxsq_str):
+def test_Poller_SNMP__parse_str_01(netsnmp_Oxsq_str):
     snmp = SNMP(community='public', host='10.0.0.1')
-    test_result = snmp._parse(netsnmp_Oxsq_str)
+    test_result = snmp._parse(netsnmp_Oxsq_str, options='Oxsq')
     result_correct = {10101: 'GigabitEthernet0/1', 10102: 'GigabitEthernet0/2'}
     assert test_result==result_correct
 
-def test_Poller_SNMP__parse_02(netsnmp_sq_str):
+def test_Poller_SNMP__parse_str_02(netsnmp_sq_str):
     snmp = SNMP(community='public', host='10.0.0.1')
-    test_result = snmp._parse(netsnmp_sq_str)
+    test_result = snmp._parse(netsnmp_sq_str, options='sq')
     result_correct = {10101: 'GigabitEthernet0/1', 10102: 'GigabitEthernet0/2'}
     assert test_result==result_correct
 
+def test_Poller_SNMP__parse_int_01(netsnmp_Oxsq_int):
+    snmp = SNMP(community='public', host='10.0.0.1')
+    test_result = snmp._parse(netsnmp_Oxsq_int, options='Oxsq')
+    result_correct = {10101: 436212224, 10102: 436212227}
+    assert test_result==result_correct
+
+
+def test_Poller_SNMP__parse_int_02(netsnmp_sq_int):
+    snmp = SNMP(community='public', host='10.0.0.1')
+    test_result = snmp._parse(netsnmp_sq_int, options='sq')
+    result_correct = {10101: 436212224, 10102: 436212227}
+    assert test_result==result_correct
